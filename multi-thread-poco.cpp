3ecb4fd0306f4e557223dac28968a86179964c54
Sunbul Kubra Yazici <sunbul@Sunbul-MacBook-Air.local>
2024-02-20 10:04:07 +0300

    poco server running as Asynchronous program on apart thread from main thread

--- multi-thread-poco.cpp
+++ multi-thread-poco.cpp
@@ -16,33 +16,22 @@ using namespace Poco;
 using namespace Poco::Net;
 using namespace Poco::Util;
 
-// Define a class for counting
-class CountRunnable : public Runnable {
-public:
-    void run() override {
-        for (int i = 1; i <= 35; ++i) {
-            std::cout << "Count: " << i << endl;
-            Thread::sleep(1000); // Sleep for 1 second
-        }
-    }
-};
-
 // Define a request handler for the Poco HTTP server
 class PocoRequestHandler : public HTTPRequestHandler {
 public:
     void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override {
-        // Log the request details to the terminal
+         // Log the request details to the terminal
         cout << "Received Request:" << endl;
         cout << "Method: " << request.getMethod() << endl;
         cout << "URI: " << request.getURI() << endl;
         cout << "HTTP Version: " << request.getVersion() << endl;
         cout << "Client Address: " << request.clientAddress().toString() << endl;
-
+        
         // Respond to the client
         response.setStatus(HTTPResponse::HTTP_OK);
         response.setContentType("text/html");
         ostream& ostr = response.send();
-        ostr << "Hello, this is Poco REST server!!!";
+        ostr << "Hello, this is Poco REST server!";
     }
 };
 
@@ -54,40 +43,50 @@ public:
     }
 };
 
-class MyApp : public ServerApplication {
-protected:
-    int main(const vector<string>& args) override {
-        // Create and start the counting thread
-        CountRunnable countRunnable;
-        Thread countThread;
-        countThread.start(countRunnable);
+// Define a class that encapsulates the server behavior
+class PocoServerThread : public Runnable {
+private:
+    bool shouldStop;
 
-        // Create and start the Poco HTTP server
+public:
+    PocoServerThread() : shouldStop(false) {}
+
+    void run() override {
         try {
             HTTPServerParams* pParams = new HTTPServerParams;
-            HTTPServer server(new PocoRequestHandlerFactory, ServerSocket(8001), pParams);
+            HTTPServer server(new PocoRequestHandlerFactory, ServerSocket(8000), pParams);
+            cout << "Before starting Poco HTTP server..." << endl;
             server.start();
-            std::cout << "Starting Poco HTTP server on port 8001..." << endl;
-
-            // Wait for CTRL-C or SIGINT
-            waitForTerminationRequest();
-
-            // Stop the server
+            cout << "Starting Poco HTTP server on port 8000..." << endl;
+            while (!shouldStop) {
+                Thread::sleep(1000);
+            }
             server.stop();
-
-            // Wait for the counting thread to finish
-            countThread.join();
-
-            return Application::EXIT_OK;
         } catch (Exception& e) {
             cerr << "Error: " << e.displayText() << endl;
-            return Application::EXIT_SOFTWARE;
         }
+    }
+
+    void stop() {
+        shouldStop = true;
     }
 };
 
 int main(int argc, char** argv) {
-    MyApp app;
-    std::cout<<"Starting App"<<std::endl;
-    return app.run(argc, argv);
+    // Create and start the Poco server in a separate thread
+    PocoServerThread serverThread;
+    Thread thread;
+    thread.start(serverThread);
+
+    // Count in the main thread
+    for (int i = 1; i <= 100; ++i) {
+        cout << "Count: " << i << endl;
+        Thread::sleep(1000); // Sleep for 1 second
+    }
+
+    // Stop the server thread
+    serverThread.stop();
+    thread.join();
+
+    return 0;
 }
